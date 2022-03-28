9fea07026af3a69368cb70d5a8604d9f4f279b87
khairilananwar17 <kanwar@hijra.id>
2022-03-28 09:36:56 +0700

    add checkout.tsx

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,13 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import CartContext from "../../store/cart-contex";
 import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
+import Checkout from "./Checkout";
 
 type Props = {
-    onClose: (event: React.MouseEvent) => void;    
-}
+    onClose: (event: React.MouseEvent) => void;    }
 const Cart: React.FC<Props> = (props: Props) => {
+
+    const [ isCheckout, setIsCheckout ] = useState<boolean>(false);
     const cartCtx = useContext(CartContext);
 
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -19,11 +21,22 @@ const Cart: React.FC<Props> = (props: Props) => {
     const cartItemAddHandler = (item: any) => {
         cartCtx.addItem(item)
     }
+    
+    const orderHandler = () => {
+        setIsCheckout(true);
+    }
     const CartItems = (
         <ul className={classes['cart-items']}> 
             {cartCtx.items.map((item) => (<CartItem key={item.id} name={item.name} price={item.price} amount={item.amount} onRemove={cartItemRemoveHandler.bind(null, item.id)} onAdd={cartItemAddHandler.bind(null, item)} />))}
         </ul>
         )
+ const modalAction = (
+     <div className={classes.actions}>
+        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
+            {hasItem && <button className={classes.button} onClick={orderHandler}>Order</button>}
+     </div>
+  )
+
  return (
     <Modal onClose={props.onClose}>
         {CartItems}
@@ -31,10 +44,8 @@ const Cart: React.FC<Props> = (props: Props) => {
             <span>Total Amount</span>
             <span>{totalAmount}</span>
         </div>
-        <div className={classes.actions}>
-            <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-            {hasItem && <button className={classes.button}>Order</button>}
-        </div>
+        {isCheckout && <Checkout/>}
+        {!isCheckout && modalAction}
     </Modal>
  )
 }
