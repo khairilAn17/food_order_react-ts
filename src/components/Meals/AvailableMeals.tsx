b4e8d4c0d38ca559d7f6ce333a9cad78c8080636
khairilananwar17 <kanwar@hijra.id>
2022-03-28 08:35:42 +0700

    update

--- src/components/Meals/AvailableMeals.tsx
+++ src/components/Meals/AvailableMeals.tsx
@@ -1,36 +1,61 @@
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
+import { useEffect, useState } from "react";
 
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
+type TMeals = {
+    id: string;
+    name: string;
+    description: string;
+    price: number
+}
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} id={meal.id}/>);
+  const [ meals, setMeals] = useState<TMeals[]>([]);
+  const [ isLoading, setIsLoading ] = useState<boolean>(false);
+  const [ httpError, setHttpError ] = useState<any>(null);
+
+  const fetchMeals = async () => {
+    setIsLoading(true);
+    const res = await fetch("https://kue-kita-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json");
+    if(!res.ok){
+      throw new Error("something went wrong");
+    }
+    const resData = await res.json();
+
+    const loadMeals = [];
+    for(const key in resData){
+      loadMeals.push({
+        id: key,
+        name: resData[key].name,
+        description: resData[key].description,
+        price: resData[key].price
+      })
+    }
+    setMeals(loadMeals);
+    setIsLoading(false);
+  }
+  useEffect(() => { 
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+
+  }, []);
+    const mealsList = meals.map(meal => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} id={meal.id}/>);
+
+    if(isLoading){
+      return <section className={classes.MealsLoading}>
+        Loading...
+      </section>
+    }
+
+    if(httpError){
+      return <section className={classes.MealsError}>
+        {httpError}
+      </section>
+    }
+
     return <section className={classes.meals}>
         <Card>
         <ul>
--- src/store/CartProvider.tsx
+++ src/store/CartProvider.tsx
@@ -42,24 +42,24 @@ const cartReducer = (state: TDefaultCartState, action: TAction): TDefaultCartSta
             totalAmount: updatedTotalAmount
         }
     }
-    // if(action.type === 'REMOVE'){
-    //     const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
+    if(action.type === 'REMOVE'){
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
 
-    //     const existingItem = state.items[existingCartItemIndex];
-    //     const updatedTotalAmount = state.totalAmount - existingItem.price;
-    //     let updatedItems;
-    //     if(existingItem.amount === 1){
-    //         updatedItems = state.items.filter(item => item.id !== action.id);
-    //     } else {
-    //         const updatedItem = { ...existingItem, amount: existingItem.amount -1 };
-    //         updatedItems = [ ...state.items ];
-    //         updatedItems[existingCartItemIndex] = updatedItem;
-    //     }
-    //     return {
-    //         items: updatedItems,
-    //         totalAmount: updatedTotalAmount
-    //     }
-    // }
+        const existingItem = state.items[existingCartItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+        let updatedItems;
+        if(existingItem.amount === 1){
+            updatedItems = state.items.filter(item => item.id !== action.id);
+        } else {
+            const updatedItem = { ...existingItem, amount: existingItem.amount -1 };
+            updatedItems = [ ...state.items ];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
     return defaultCartState;
 }
 const CartProvider: React.FC<Props> = (props: Props) => {
