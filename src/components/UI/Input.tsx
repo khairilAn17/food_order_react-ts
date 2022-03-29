30c166eeb5b45212b8149f434b01bb2ea8afa866
khairilananwar17 <kanwar@hijra.id>
2022-03-29 13:21:06 +0700

    update

--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -14,10 +14,12 @@ type Props = {
 }
 
 const Input = React.forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
-    return <div className={classes.input}>
-        <label htmlFor={props.input.id}>{props.label}</label>
-        <input ref={ref} {...props.input} />
-    </div>
+    return (
+        <div className={classes.input}>
+            <label htmlFor={props.input.id}>{props.label}</label>
+            <input ref={ref} {...props.input} />
+        </div>
+        )
 })
 
 export default Input;
